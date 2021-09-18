aee445c847100d1655ef6bf1a098562a1e59aca2
Ariadna <achicol1900@alumno.ipn.mx>
2021-09-18 15:28:28 -0500

    funcionaaaaaaaa

--- js/omniman.js
+++ js/omniman.js
@@ -3,3 +3,21 @@ let start = img.src;
 
 img.onmouseover = () => { img.src = './img/thick/thiccomniman.png'; }
 img.onmouseout = () => { img.src = start; } //to revert back to start
+
+
+var playBtn = document.getElementById('brrr'),
+  hearbeat = document.getElementById('beep')
+	audios = document.querySelectorAll('audio');
+console.log(audios);
+
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  // do whatever
+  audio.play();
+});
+}, false);
+
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
